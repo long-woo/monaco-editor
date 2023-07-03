e3a66205b6dc629a39fb21c151c2ad4a633f6fb9
loong.woo <longwunet@outlook.com>
2023-07-03 07:58:24 +0000

    feat: add vue.

---
+++ src/basic-languages/vue/vue.ts
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import type { languages } from '../../fillers/monaco-editor-core';
+
+export const conf: languages.LanguageConfiguration = {
+	comments: {
+		lineComment: '//',
+		blockComment: ['/*', '*/']
+	},
+
+	brackets: [
+		['{', '}'],
+		['[', ']'],
+		['(', ')'],
+		// html
+		['<!--', '-->'],
+		['<', '>'],
+		['{{', '}}']
+	]
+};
